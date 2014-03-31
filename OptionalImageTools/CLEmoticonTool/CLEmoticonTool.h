009545d95a2bd38ec8e55990cc347cf3afd767f8
iMokhles <mokhleshussien@aol.com>
2014-03-31 15:01:19 +0200

    Added CLEmoticonTool
    This tool used in Rendarya Project with some modifications.
    www.rendarya.idevttweaks.com

---
+++ OptionalImageTools/CLEmoticonTool/CLEmoticonTool.h
@@ -0,0 +1,13 @@
+//
+//  CLEmoticonTool.h
+//
+//  Created by Mokhlas Hussein on 01/02/14.
+//  Copyright (c) 2014 iMokhles. All rights reserved.
+//  CLImageEditor Author sho yakushiji.
+//
+
+#import "CLImageToolBase.h"
+
+@interface CLEmoticonTool : CLImageToolBase
+
+@end
